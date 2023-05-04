acd69e8256ab63636b7ed917208ae1ab159c5373
nalanridlo <nalanridlof@student.ub.ac.id>
2023-05-04 18:10:19 +0700

    javascript login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,73 @@
     
-    //hidden password function 1
-    const passwordInput = document.getElementById('passwordInput'); // id dari input password
-    const showHide = document.getElementById('showHide'); // id span showHide dalam input group password
+    // //hidden password function 1
+    // const passwordInput = document.getElementById('passwordInput'); // id dari input password
+    // const showHide = document.getElementById('showHide'); // id span showHide dalam input group password
 
-    passwordInput.type = 'password'; // set type input password menjadi password
-    showHide.innerHTML = '<i class="fa fa-eye">'; // masukan icon eye dalam icon bootstrap 5
-    showHide.style.cursor = 'pointer'; // ubah cursor menjadi pointer
-    // jadi ketika span di hover maka cursornya berubah pointer
+    // passwordInput.type = 'password'; // set type input password menjadi password
+    // showHide.innerHTML = '<i class="fa fa-eye">'; // masukan icon eye dalam icon bootstrap 5
+    // showHide.style.cursor = 'pointer'; // ubah cursor menjadi pointer
+    // // jadi ketika span di hover maka cursornya berubah pointer
 
-    showHide.addEventListener('click', () => {
-    // ketika span diclick
-        if (passwordInput.type === 'password') {
-            // jika type inputnya password
-            passwordInput.type = 'text'; // ubah type menjadi text
-            showHide.innerHTML = '<i class="fa fa-eye-slash">'; // ubah icon menjadi eye slash
-        } else {
-            // jika type bukan password (text)
-            showHide.innerHTML = '<i class="fa fa-eye">'; // ubah icon menjadi eye
-            passwordInput.type = 'password'; // ubah type menjadi password
-        }
-    });
+    // showHide.addEventListener('click', () => {
+    // // ketika span diclick
+    //     if (passwordInput.type === 'password') {
+    //         // jika type inputnya password
+    //         passwordInput.type = 'text'; // ubah type menjadi text
+    //         showHide.innerHTML = '<i class="fa fa-eye-slash">'; // ubah icon menjadi eye slash
+    //     } else {
+    //         // jika type bukan password (text)
+    //         showHide.innerHTML = '<i class="fa fa-eye">'; // ubah icon menjadi eye
+    //         passwordInput.type = 'password'; // ubah type menjadi password
+    //     }
+    // });
 
-    //hidden confirm password 2
-    const konfirmasiPassword = document.getElementById('konfirmasiPassword'); // id dari input password
-    const showHidePassword = document.getElementById('showHidePassword'); // id span showHide dalam input group password
+    // //hidden confirm password 2
+    // const konfirmasiPassword = document.getElementById('konfirmasiPassword'); // id dari input password
+    // const showHidePassword = document.getElementById('showHidePassword'); // id span showHide dalam input group password
 
-    konfirmasiPassword.type = 'password'; // set type input password menjadi password
-    showHidePassword.innerHTML = '<i class="fa fa-eye">'; // masukan icon eye dalam icon bootstrap 5
-    showHidePassword.style.cursor = 'pointer'; // ubah cursor menjadi pointer
-    // jadi ketika span di hover maka cursornya berubah pointer
+    // konfirmasiPassword.type = 'password'; // set type input password menjadi password
+    // showHidePassword.innerHTML = '<i class="fa fa-eye">'; // masukan icon eye dalam icon bootstrap 5
+    // showHidePassword.style.cursor = 'pointer'; // ubah cursor menjadi pointer
+    // // jadi ketika span di hover maka cursornya berubah pointer
 
-    showHidePassword.addEventListener('click', () => {
-    // ketika span diclick
-        if (konfirmasiPassword.type === 'password') {
-            // jika type inputnya password
-            konfirmasiPassword.type = 'text'; // ubah type menjadi text
-            showHidePassword.innerHTML = '<i class="fa fa-eye-slash">'; // ubah icon menjadi eye slash
-        } else {
-            // jika type bukan password (text)
-            showHidePassword.innerHTML = '<i class="fa fa-eye">'; // ubah icon menjadi eye
-            konfirmasiPassword.type = 'password'; // ubah type menjadi password
-        }
-    });
+    // showHidePassword.addEventListener('click', () => {
+    // // ketika span diclick
+    //     if (konfirmasiPassword.type === 'password') {
+    //         // jika type inputnya password
+    //         konfirmasiPassword.type = 'text'; // ubah type menjadi text
+    //         showHidePassword.innerHTML = '<i class="fa fa-eye-slash">'; // ubah icon menjadi eye slash
+    //     } else {
+    //         // jika type bukan password (text)
+    //         showHidePassword.innerHTML = '<i class="fa fa-eye">'; // ubah icon menjadi eye
+    //         konfirmasiPassword.type = 'password'; // ubah type menjadi password
+    //     }
+    // });
 
+
+
+    // fetch data from JSON file
+fetch('data.json')
+.then(response => response.json())
+.then(data => {
+  const loginForm = document.getElementById('loginForm');
+  
+
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault(); // prevent default form submit behavior
+
+    const email = loginForm.elements.email.value;
+    const password = loginForm.elements.password.value;
+
+    // find user with matching username and password
+    const user = data.users.find(user => user.email === email && user.password === password);
+
+    if (user) {
+      // alert(`Welcome, ${user.name}!`);
+      // loginForm.reset();
+      window.location.replace("index.html");
+    } else {
+      alert('Invalid username or password.');
+    }
+  });
+})
+.catch(error => console.error(error));
